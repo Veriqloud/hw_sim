c5d830bdd4725de1797f0c45139d13b4a45c9cb1
Rémi CORNIERE <remi.corniere@veriqloud.fr>
2026-03-16 15:45:17 +0100

    removed genericity for sim

--- simulator/src/backend/mod.rs
+++ simulator/src/backend/mod.rs
@@ -5,8 +5,3 @@ pub mod role;
 pub mod simulation;
 #[cfg(test)]
 pub mod tests;
-
-use self::simulation::VqSim;
-
-pub trait BytesGenerator: VqSim + Send + Sync + 'static {}
-impl<T> BytesGenerator for T where T: VqSim + Send + Sync + 'static {}
